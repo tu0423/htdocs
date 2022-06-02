a52bf83cc92276bfe9731bdfae5f9a857c515426
tu0423 <bakiogregonnchann@gmail.com>
2022-06-02 18:49:45 +0900

    master

--- lesson01/sample02.php
+++ lesson01/sample02.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sample01</title>
+    <title>sample02</title>
 </head>
 <body>
     <?php
     echo 'I\'m php勉強中';
     ?>
     <?php
-    echo date('G時 i分 s秒');
+    date_default_timezone_set('Asia/Tokyo');
+    echo '現在は、'. date('G時 i分 s秒') .'です。';
     ?>
 </body>
 </html>
---
+++ lesson01/sample03.php
@@ -0,0 +1,11 @@
+
+
+<?php
+date_default_timezone_set('Asia/Tokyo');
+$time = date('G');
+?>
+<?php if ($time < 9): ?>
+    <p>*営業時間外です</p>
+<?php else: ?>
+    <p>ようこそ</p>
+<?php endif; ?>
